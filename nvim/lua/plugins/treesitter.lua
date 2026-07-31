44412d4f4aa6482d8fc6dba5095e94a368c861c8
JacobEscoto <djacobescotoz@gmail.com>
2026-07-31 04:48:26 -0600

    update(nvim): add new languages to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,19 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
+    build = ":TSUpdate",
     config = function()
+      local ts = require("nvim-treesitter")
+      ts.setup()
+      ts.install({ "bash", "lua", "go", "vim", "vimdoc", "json", "markdown" })
+
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "go", "lua", "sh" },
+        pattern = { "bash", "sh", "lua", "go", "vim", "vimdoc", "json", "markdown" },
         callback = function()
           vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
       })
     end,
