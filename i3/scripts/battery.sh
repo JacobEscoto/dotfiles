bfb422cac583c3f036466d3ab3c39b693bc2fa67
JacobEscoto <djacobescotoz@gmail.com>
2026-08-30 17:37:25 -0600

    i3: add battery monitor script on startup

---
+++ i3/scripts/battery.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Ensure it only exists one instance
+pkill -f "battery.sh" 2>/dev/null
+sleep 1
+
+notified_20=false
+notified_10=false
+notified_5=false
+
+while true; do
+  battery=$(cat /sys/class/power_supply/BAT*/capacity 2>/dev/null | head -1)
+  status=$(cat /sys/class/power_supply/BAT*/status 2>/dev/null | head -1)
+
+  # Full charge notification
+  if [ "$battery" -ge 95 ] && [ "$status" == "Charging" ]; then
+    notify-send -u critical "Battery" "Complete charge at ${battery}%"
+    sleep 300
+  fi
+
+  # Reset notifications when charging
+  if [ "$status" == "Charging" ]; then
+    notified_20=false
+    notified_10=false
+    notified_5=false
+  fi
+
+  if [ "$battery" -le 20 ] && [ "$battery" -gt 10 ] && [ "$notified_20" = false ]; then
+    notify-send -u critical "Low battery" "Only ${battery}% available"
+    notified_20=true
+  fi
+
+  if [ "$battery" -le 10 ] && [ "$battery" -gt 5 ] && [ "$notified_10" = false ]; then
+    notify-send -u critical "Low battery" "Only ${battery}% available"
+    notified_10=true
+  fi
+
+  if [ "$battery" -le 5 ] && [ "$notified_5" = false ]; then
+    notify-send -u critical "Critical battery" "Only ${battery}% available - Shutdown soon!"
+    notified_5=true
+  fi
+  sleep 30
+done
