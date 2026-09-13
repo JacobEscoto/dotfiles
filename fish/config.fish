01f6db4fdd9241b951521e46faa387da1b8fac15
JacobEscoto <djacobescotoz@gmail.com>
2026-09-13 11:19:25 -0600

    update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,62 +1,58 @@
 # Disable initial greeting
 set -g fish_greeting ""
 
-# Abbreviations for system commands
-abbr -a cat bat
-abbr -a reload "exec fish"
-abbr -a fishcfg "nvim ~/.config/fish/config.fish"
+# Environment variables
+set -gx MICRO_TRUECOLOR 1
+set -gx XDG_MENU_PREFIX plasma-
 
-# File list (eza)
-abbr -a ls "eza --icons=always --group-directories-first"
-abbr -a ll "eza --icons=always --group-directories-first --long --header --git --time-style=relative"
-abbr -a la "eza --icons=always --group-directories-first --long --header --git --all"
-abbr -a tree "eza --icons=always --tree --level=3 --ignore-glob='node_modules|.git'"
-abbr -a treell "eza --icons=always --tree --long --header --git --ignore-glob='node_modules|.git'"
-abbr -a lsd "eza --icons=always --only-dirs"
-abbr -a lsf "eza --icons=always --only-files"
+# Add local and Go binaries to PATH
+fish_add_path ~/.local/bin
+fish_add_path ~/go/bin
 
-# Environment Configuration
+# Linuxbrew environment setup
 if test -d /home/linuxbrew/.linuxbrew
     /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 end
 
-# Start ssh-agent and load private key if not active
-if not set -q SSH_AUTH_SOCK
-  eval (ssh-agent -c) > /dev/null
-  ssh-add ~/.ssh/id_22519 2> /dev/null
-end
-
-# Enable true color for Micro Editor
-set -gx MICRO_TRUECOLOR 1
-
-# Add local binaries and go binaries to PATH
-set -gx PATH "$HOME/.local/bin" "$HOME/go/bin" $PATH
-
-# Syntax Colors
-set -g fish_color_command 76abdf
-set -g fish_color_param c9ffe5
-set -g fish_color_keyword cba6f7
-set -g fish_color_quote a5b4fc
-set -g fish_color_redirection 0492c2
-set -g fish_color_end 76abdf
-set -g fish_color_error fb4934
-set -g fish_color_autosuggestion 475569
-
-set -g fish_color_search_match --background=151e3d
-set -g fish_color_selection --background=151e3d
-set -g fish_color_operator 0492c2
-set -g fish_color_escape cba6f7
-
-# Ensures to add `.local/bin` and `linuxbrew/bin` to the fish path
-fish_add_path ~/.local/bin
-fish_add_path /home/linuxbrew/.linuxbrew/bin
-
-# Initialize Zoxide
-zoxide init fish | source
-
-# Initialize Starship
-starship init fish | source
-
 if status is-interactive
+  # Abbreviations for system commands
+  abbr -a reload "exec fish"
+  abbr -a fish-config "nvim ~/.config/fish/config.fish"
+
+  # File list (eza)
+  abbr -a ls "eza --icons=always --group-directories-first"
+  abbr -a ll "eza --icons=always --group-directories-first --long --header --git --time-style=relative"
+  abbr -a la "eza --icons=always --group-directories-first --long --header --git --all"
+  abbr -a tree "eza --icons=always --tree --level=3 --ignore-glob='node_modules|.git|build|.cache'"
+  abbr -a treell "eza --icons=always --tree --long --header --git --ignore-glob='node_modules|.git|build|.cache'"
+  abbr -a lsd "eza --icons=always --only-dirs"
+  abbr -a lsf "eza --icons=always --only-files"
+
+  # Syntax Colors
+  set -g fish_color_command 76abdf
+  set -g fish_color_param c9ffe5
+  set -g fish_color_keyword cba6f7
+  set -g fish_color_quote a5b4fc
+  set -g fish_color_redirection 0492c2
+  set -g fish_color_end 76abdf
+  set -g fish_color_error fb4934
+  set -g fish_color_autosuggestion 475569
+
+  set -g fish_color_search_match --background=151e3d
+  set -g fish_color_selection --background=151e3d
+  set -g fish_color_operator 0492c2
+  set -g fish_color_escape cba6f7
+
+  # Start SSH agent
+  if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_22519 2> /dev/null
+  end
+
+  # Interactive tools
+  zoxide init fish | source
+  starship init fish | source
+
+  # Welcome
   fastfetch
 end
